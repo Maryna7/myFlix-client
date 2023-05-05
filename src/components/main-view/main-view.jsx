f6339fa899d5a96b6195e75fde23f0314e14d182
Maryna7 <m.kurman23@gmail.com>
2023-05-05 11:15:41 +0200

    Create a new branch 'dev-branch'. Load movies data from an API. Use PropTypes for updated components. Display similar movies on each MovieView component.

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,52 +1,49 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
-  const [movies, setMovies] = useState([
-    {
-      id: 1,
-      image: "https://m.media-amazon.com/images/I/81+H4lZVw+L._AC_UF894,1000_QL80_.jpg",
-      title: "Schindler's List",
-      description: "In German-occupied Poland during World War II, industrialist Oskar Schindler gradually becomes concerned for his Jewish workforce after witnessing their persecution by the Nazis.",
-      genre: "History",
-      director: {
-        name: 'Steven Spielberg',
-        bio: 'One of the most influential personalities in the history of cinema, Steven Spielberg is Hollywood\'s best known director and one of the wealthiest filmmakers in the world. He has an extraordinary number of commercially successful and critically acclaimed credits to his name, either as a director, producer or writer since launching the summer blockbuster with Jaws (1975), and he has done more to define popular film-making since the mid-1970s than anyone else.',
-        birth: 1946
-      }
-    },
-    {
-      id: 2,
-      image: "https://m.media-amazon.com/images/I/91++WV6FP4L._SL1500_.jpg",
-      title: "Forrest Gump",
-      description: "The presidencies of Kennedy and Johnson, the Vietnam War, the Watergate scandal and other historical events unfold from the perspective of an Alabama man with an IQ of 75, whose only desire is to be reunited with his childhood sweetheart.",
-      genre: "Romance",
-      director: {
-        name: 'Robert Zemeckis',
-        bio: '',
-        birth: 1952
-      }
-    },
-    {
-      id: 2,
-      image: "https://m.media-amazon.com/images/I/91HHTI-Yt4L._SY445_.jpg",
-      title: "The Grand Budapest Hotel",
-      description: "A writer encounters the owner of an aging high-class hotel, who tells him of his early years serving as a lobby boy in the hotel\'s glorious years under an exceptional concierge.",
-      genre: "Adventure",
-      director: {
-        name: 'Wes Anderson',
-        bio: '',
-        birth: 1969
-      }
-    }
-  ]);
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch("https://maryna-myflix-app.herokuapp.com/movies")
+      .then((response) => response.json())
+      .then((data) => {
+        const moviesFromApi = data.map((doc) => {
+          return {
+            id: doc._id,
+            title: doc.Title,
+            description: doc.Description,
+            image: doc.ImagePath,
+            directorName: doc.Director.Name,
+            genreName: doc.Genre.Name,
+            featured: doc.Featured
+          };
+        });
+
+        setMovies(moviesFromApi);
+      });
+  }, []);
 
   const [selectedMovie, setSelectedMovie] = useState(null);
 
   if (selectedMovie) {
+    let similarMovies = movies.filter(movie => (selectedMovie.genreName === movie.genreName) && (movie != selectedMovie));
     return (
-      <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
+      <>
+        <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
+        <hr />
+        <h2>Similar Movies</h2>
+        {similarMovies.map((movie) => (
+          <MovieCard
+            key={movie.id}
+            movie={movie}
+            onMovieClick={(newSelectedMovie) => {
+              setSelectedMovie(newSelectedMovie);
+            }}
+          />
+        ))}
+      </>
     );
   }
 
